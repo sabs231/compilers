c47122029a022cfc0e683bdb37fd486ef0f37225
Sergio Baez <sergio.baez231@gmail.com>
2013-09-28 12:00:18 -0500

    first commit

---
+++ include/AAutomata.hh
@@ -0,0 +1,27 @@
+#ifndef _AAUTOMATA_HH_
+# define _AATOMATA_HH_
+
+typedef struct 		s_state
+{
+	char 						name;
+	bool 						isFinal;
+} 								t_state;
+
+class 						AAutomata
+{
+	protected:
+		t_state 			**_trasitionTable;
+		unsigned int 	_stateNum;
+		unsigned int 	_symbolNum;
+
+	public:
+		AAutomata();
+		AAutomata(AAutomata const & other);
+		AAutomata(t_state **transitionTable, unsigned int stateNum, unsigned int symbolNum); 
+		AAutomata & operator=(AAutomata const & other);
+		virtual ~AAutomata() {}
+		char 	getInitialState() const;
+		virtual char transition(t_state *currentState, char nextChar) = 0;
+};
+
+#endif
---
+++ src/AAutomata.cpp
@@ -0,0 +1,21 @@
+#include 	"AAutomata.hh"
+
+AAutomata::AAutomata()
+{
+}
+
+AAutomata::AAutomata(AAutomata const & other)
+{
+}
+
+AAutomata::AAutomata(t_state **transitionTable, unsigned int stateNum, unsigned int symbolNum)
+{
+}
+
+AAutomata & AAutomata::operator=(AAutomata const & other)
+{
+}
+
+char 	AAutomata::getInitialState() const
+{
+}
